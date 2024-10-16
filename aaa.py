12c2bf33aa65bbd4917e27ea2fa2e3b73f4c333c
martycoder69 <martinjthekkekara@gmail.com>
2024-10-16 11:03:59 +0530

    initial commit

---
+++ aaa.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number:"))
+if a%2==0:
+    a+=10
+    print("the entered number is even number")
+    print(a)
+else:
+    a+=5
+    print("the entered number is odd number")
+    print(a)
---
+++ ccc.py
@@ -0,0 +1,7 @@
+a=int(input("enter your marks in maths:"))
+b=int(input("enter your marks in english:"))
+c=int(input("enter your marks in physics:"))
+d=int(input("enter your marks in chemistry:"))
+e=int(input("enter your marks in biology:"))
+f=(a+b+c+d+e)
+print(f/5)
