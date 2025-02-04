4f8d2a00325f88f030259bded8101db0486c8902
Martycoder69 <martinjthekkekara@gmail.com>
2025-02-04 11:05:50 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import sqlite3
-
-
-sqliteConnection = sqlite3.connect('my.db')
-cursor = sqliteConnection.cursor()
-print('DB Init')
-table = """ CREATE TABLE GEEK (
-            Email VARCHAR(255) NOT NULL,
-            First_Name CHAR(25) NOT NULL,
-            Last_Name CHAR(25),
-            Score INT
-        ); """
+ 
+connection = sqlite3.connect('my.db')
+ 
+cursor = connection.cursor()
+ 
+table = """INSERT INTO Studentinfo (First_Name,Last_Name, Email, Score, Age) 
+VALUES ('Lewis','Hamilton', 'lewishamilton@gmail.com', 65, 30);
+"""
  
 cursor.execute(table)
  
 print("Table is Ready")
- 
-sqliteConnection.close()
+
+
+connection.commit()
+connection.close()
